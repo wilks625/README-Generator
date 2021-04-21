f7f9c3bf569aa099932dbf30204ed222ee6d8e92
Stephen Yacas <syacas123@gmail.com>
2021-04-20 22:40:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
